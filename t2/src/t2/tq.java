4c10fb7911da21571b76349db3e181cd9f6bfbdb
zhujn <43258005+zhujnan@users.noreply.github.com>
2020-05-18 17:33:42 +0800

    新增123
    在第二个输出语句中添加123

--- t2/src/t2/tq.java
+++ t2/src/t2/tq.java
@@ -5,7 +5,7 @@ public class tq {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("你好呀");
-			System.out.println();
+			System.out.println("123");
 	}
 
 }
