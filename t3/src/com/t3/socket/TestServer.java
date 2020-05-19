2391857dc790253083d910144fe11bd621ead085
zhujnan <247176894@qq.com>
2020-05-19 13:47:28 +0800

    添加ServerSocket
    Signed-off-by: zhujnan <247176894@qq.com>

---
+++ t3/src/com/t3/socket/TestServer.java
@@ -0,0 +1,15 @@
+package com.t3.socket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class TestServer {
+
+	public static void main(String[] args) 
+			throws IOException {
+		ServerSocket serverSocket
+			= new ServerSocket(10087);
+		
+	}
+
+}
